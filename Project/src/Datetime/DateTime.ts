1043f98e8bf830302cbe6626c2dede2d04521847
VIREAK.ROEUN <VIREAK.ROEUN@PNC.LAN>
2023-04-28 20:49:34 +0700

    create class time

--- Project/src/Datetime/DateTime.ts
+++ Project/src/Datetime/DateTime.ts
@@ -1,3 +1,7 @@
 export class DateTime {
-    constructor(private day: number, private month: number, private year: number, private time: number ){}
+    constructor(private day: number, private month: number, private year: number, private time: Time ){}
+}
+
+export class Time {
+    constructor(private hour: number, private minute: number){}
 }
